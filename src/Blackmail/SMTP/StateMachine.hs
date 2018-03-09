aeb32618fbb614ced9ece2db4f6ddf1031f226f0
Alexis Williams <alexis@typedr.at>
2018-03-09 00:20:30 -0800

    Take out pipes in preparation to switch to conduit

--- src/Blackmail/SMTP/StateMachine.hs
+++ src/Blackmail/SMTP/StateMachine.hs
@@ -1,12 +1,8 @@
 module Blackmail.SMTP.StateMachine (module Blackmail.SMTP.StateMachine.Types) where
 
-import Control.FSM.Monad
-import Control.Lens
-import qualified Data.ByteString as BS
-
 import Blackmail.SMTP.Address
 import Blackmail.SMTP.StateMachine.Types
-
---
+import Blackmail.SMTP.WireProtocol
+import Blackmail.ServerCore
 
 
--- src/Blackmail/ServerCore.hs
+++ src/Blackmail/ServerCore.hs
@@ -1,17 +1,1 @@
-module Blackmail.ServerCore (ConnectionHandler, runServer) where
-
-import qualified Data.ByteString.Char8 as BS
-import Pipes
-import Pipes.Safe
-import Pipes.Network.TCP.Safe
-
-type ConnectionHandler m = forall r. (Monad m) => (Producer BS.ByteString m (), Consumer BS.ByteString m r, SockAddr) -> m ()
--- ^ The type of a simple connection handler. Just a function from a 'Producer' for the received data, a 'Consumer' for the data
---   to be transmitted, and the 'SockAddr' of the client to a monadic action returning ().
-
-runServer :: (MonadSafe m) => HostPreference -> ServiceName -> ConnectionHandler IO -> m ()
-runServer host port server = serve host port $ \(sock, addr) -> do
-    let recv = fromSocket sock 4096
-    let send = toSocket sock
-    server (recv, send, addr)
--- ^ Given a hostname and port to listen on, loop infinitely and handle each new connection with the connection handler.
+module Blackmail.ServerCore () where
